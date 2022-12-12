b2639ebf950e258e48c9e7266f480fe880092dc6
JidapaBubu <jidapa4499@gmail.com>
2022-12-12 23:27:18 +0700

    up

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Tree<string> t = new Tree<string>();
+        string name;
+
+    }
+    
+    /*สุดความสามารถแล้วจริวค่ะอาจาร์ย ขออนุญาตเขียนอธิบาย
+
+    ต้องทำการเขียนโปรแกรมรับค่ามา สมมติตั้งตัวแปรเป็น name และ day 
+    ทำการเขียนให้รับค่าเก็บำว้ใน treenode เก็บค่าตัวแปรไว้ด้วย ref
+
+    หลักการสำคัญคือ สร้างโหนดว่างๆต่อจากโหนดหัวหน้า ตอนเพิ่มพนักงานก็ให้วิ่งไปหาโหนดว่างก่อน แล้วเพิ่มต่อจากโหนดว่างนั้น*/
+    
+}
+
